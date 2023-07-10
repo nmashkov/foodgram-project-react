1ffe84f976dce0c551a9b0e709e345609bcfcad2
Никита Машков <nmashkov1997@yandex.ru>
2023-07-10 11:48:33 +0300

    initial project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # praktikum_new_diplom
+
+## Описание
+
+Дипломный проект.
+
+## Используемые библиотеки
+
+- Python 3.9.10
+- Django 3.2.3
+- Django Rest Framework 3.12.4
+- Djoser 2.1.0
+
+## Автор
+
+Никита Машков
---
+++ backend/foodgram_backend/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
